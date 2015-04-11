b8dc3418cc4535b0478bedfce0a2b9b1ced79a76
kcaluru <krishna_aluru@brown.edu>
2015-04-11 15:07:11 +0400

    added iframe

--- Extension/script/main.js
+++ Extension/script/main.js
@@ -1,18 +1,26 @@
-var simulateClick = function(element) {
-  var dispatchEvent = function (elt, name) {
-    var clickEvent = document.createEvent('MouseEvents');
-    clickEvent.initEvent(name, true, true);
-    elt.dispatchEvent(clickEvent);
-  };
-  dispatchEvent(element, 'mouseover');
-  dispatchEvent(element, 'mousedown');
-  dispatchEvent(element, 'click');
-  dispatchEvent(element, 'mouseup');
-};
+// var simulateClick = function(element) {
+//   var dispatchEvent = function (elt, name) {
+//     var clickEvent = document.createEvent('MouseEvents');
+//     clickEvent.initEvent(name, true, true);
+//     elt.dispatchEvent(clickEvent);
+//   };
+//   dispatchEvent(element, 'mouseover');
+//   dispatchEvent(element, 'mousedown');
+//   dispatchEvent(element, 'click');
+//   dispatchEvent(element, 'mouseup');
+// };
 
-simulateClick(document.getElementsByClassName("ytp-button ytp-button-volume")[0]);
-simulateClick(document.getElementsByClassName("ytp-button ytp-button-play")[0]);
+// simulateClick(document.getElementsByClassName("ytp-button ytp-button-volume")[0]);
+// simulateClick(document.getElementsByClassName("ytp-button ytp-button-play")[0]);
 
 var iframe = document.createElement('iframe');
-iframe.src = "../microphone.html";
+iframe.src = chrome.extension.getURL("microphone.html");
+iframe.style.position = 'fixed';
+iframe.style.top = '50px';
+iframe.style.right = '20px';
+iframe.style.height = '50px';
+iframe.style.width = '50px';
+iframe.style.background = 'blue';
+iframe.style.zIndex = '9999';
+
 document.body.appendChild(iframe);
