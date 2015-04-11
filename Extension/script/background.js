06b23ce4d782d2c3a2a6eff54546d05fd507fb64
kcaluru <krishna_aluru@brown.edu>
2015-04-11 10:59:19 +0400

    added recording + download

--- Extension/script/background.js
+++ Extension/script/background.js
@@ -1,3 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript(null, {file: "js/jquery-2.1.1.min.js"});
   	chrome.tabs.executeScript(null, {file: "script/main.js"});
 });
--- Extension/script/main.js
+++ Extension/script/main.js
@@ -1,5 +1,28 @@
 var url = document.URL;
-console.log(url);
+var leftchannel = [];
+var rightchannel = [];
+var recorder = null;
+var recording = false;
+var recordingLength = 0;
+var volume = null;
+var audioInput = null;
+var sampleRate = null;
+var audioContext = null;
+var context = null;
+
+var simulateClick = function(element) {
+  var dispatchEvent = function (elt, name) {
+    var clickEvent = document.createEvent('MouseEvents');
+    clickEvent.initEvent(name, true, true);
+    elt.dispatchEvent(clickEvent);
+  };
+  dispatchEvent(element, 'mouseover');
+  dispatchEvent(element, 'mousedown');
+  dispatchEvent(element, 'click');
+  dispatchEvent(element, 'mouseup');
+};
+
+simulateClick(document.getElementsByClassName("ytp-button ytp-button-volume")[0]);
 
 if (!navigator.getUserMedia) {
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -11,40 +34,113 @@ if (navigator.getUserMedia){
     });
 } else alert('getUserMedia not supported in this browser.');
 
+// when key is down
+window.onkeydown = function(e){
+    
+    // if R is pressed, we start recording
+    if ( e.keyCode == 82 ){
+        recording = true;
+        // reset the buffers for the new recording
+        leftchannel.length = rightchannel.length = 0;
+        recordingLength = 0;
+        console.log('Recording now...');
+    // if S is pressed, we stop the recording and package the WAV file
+    } else if ( e.keyCode == 83 ){
+        
+        // we stop recording
+        recording = false;
+        
+        console.log('Building wav file...');
+
+        // we flat the left and right channels down
+        var leftBuffer = mergeBuffers ( leftchannel, recordingLength );
+        var rightBuffer = mergeBuffers ( rightchannel, recordingLength );
+        // we interleave both channels together
+        var interleaved = interleave ( leftBuffer, rightBuffer );
+        
+        // we create our wav file
+        var buffer = new ArrayBuffer(44 + interleaved.length * 2);
+        var view = new DataView(buffer);
+        
+        // RIFF chunk descriptor
+        writeUTFBytes(view, 0, 'RIFF');
+        view.setUint32(4, 44 + interleaved.length * 2, true);
+        writeUTFBytes(view, 8, 'WAVE');
+        // FMT sub-chunk
+        writeUTFBytes(view, 12, 'fmt ');
+        view.setUint32(16, 16, true);
+        view.setUint16(20, 1, true);
+        // stereo (2 channels)
+        view.setUint16(22, 2, true);
+        view.setUint32(24, sampleRate, true);
+        view.setUint32(28, sampleRate * 4, true);
+        view.setUint16(32, 4, true);
+        view.setUint16(34, 16, true);
+        // data sub-chunk
+        writeUTFBytes(view, 36, 'data');
+        view.setUint32(40, interleaved.length * 2, true);
+        
+        // write the PCM samples
+        var lng = interleaved.length;
+        var index = 44;
+        var volume = 1;
+        for (var i = 0; i < lng; i++){
+            view.setInt16(index, interleaved[i] * (0x7FFF * volume), true);
+            index += 2;
+        }
+        
+        // our final binary blob
+        var blob = new Blob ( [ view ], { type : 'audio/wav' } );
+        
+        // let's save it locally
+        console.log('Handing off the file now...');
+        var url = (window.URL || window.webkitURL).createObjectURL(blob);
+        var link = window.document.createElement('a');
+        link.href = url;
+        link.download = 'output.wav';
+        var click = document.createEvent("Event");
+        click.initEvent("click", true, true);
+        link.dispatchEvent(click);
+    }
+}
+
 function success(e){
     // creates the audio context
     audioContext = window.AudioContext || window.webkitAudioContext;
     context = new audioContext();
- 
-    // retrieve the current sample rate to be used for WAV packaging
+
+	// we query the context sample rate (varies depending on platforms)
     sampleRate = context.sampleRate;
- 
+
+    console.log('succcess');
+    
     // creates a gain node
     volume = context.createGain();
- 
+
     // creates an audio node from the microphone incoming stream
     audioInput = context.createMediaStreamSource(e);
- 
+
     // connect the stream to the gain node
     audioInput.connect(volume);
- 
+
     /* From the spec: This value controls how frequently the audioprocess event is 
     dispatched and how many sample-frames need to be processed each call. 
     Lower values for buffer size will result in a lower (better) latency. 
     Higher values will be necessary to avoid audio breakup and glitches */
     var bufferSize = 2048;
-    recorder = context.createJavaScriptNode(bufferSize, 2, 2);
- 
+    recorder = context.createScriptProcessor(bufferSize, 2, 2);
+
     recorder.onaudioprocess = function(e){
-        console.log ('recording');
+        if (!recording) return;
         var left = e.inputBuffer.getChannelData (0);
         var right = e.inputBuffer.getChannelData (1);
         // we clone the samples
         leftchannel.push (new Float32Array (left));
         rightchannel.push (new Float32Array (right));
         recordingLength += bufferSize;
+        console.log('recording');
     }
- 
+
     // we connect the recorder
     volume.connect (recorder);
     recorder.connect (context.destination); 
@@ -82,44 +178,3 @@ function writeUTFBytes(view, offset, string){
     view.setUint8(offset + i, string.charCodeAt(i));
   }
 }
-
-// we flat the left and right channels down
-var leftBuffer = mergeBuffers ( leftchannel, recordingLength );
-var rightBuffer = mergeBuffers ( rightchannel, recordingLength );
-// we interleave both channels together
-var interleaved = interleave ( leftBuffer, rightBuffer );
- 
-// create the buffer and view to create the .WAV file
-var buffer = new ArrayBuffer(44 + interleaved.length * 2);
-var view = new DataView(buffer);
- 
-// write the WAV container, check spec at: https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
-// RIFF chunk descriptor
-writeUTFBytes(view, 0, 'RIFF');
-view.setUint32(4, 44 + interleaved.length * 2, true);
-writeUTFBytes(view, 8, 'WAVE');
-// FMT sub-chunk
-writeUTFBytes(view, 12, 'fmt ');
-view.setUint32(16, 16, true);
-view.setUint16(20, 1, true);
-// stereo (2 channels)
-view.setUint16(22, 2, true);
-view.setUint32(24, sampleRate, true);
-view.setUint32(28, sampleRate * 4, true);
-view.setUint16(32, 4, true);
-view.setUint16(34, 16, true);
-// data sub-chunk
-writeUTFBytes(view, 36, 'data');
-view.setUint32(40, interleaved.length * 2, true);
- 
-// write the PCM samples
-var lng = interleaved.length;
-var index = 44;
-var volume = 1;
-for (var i = 0; i < lng; i++){
-    view.setInt16(index, interleaved[i] * (0x7FFF * volume), true);
-    index += 2;
-}
- 
-// our final binary blob that we can hand off
-var blob = new Blob ( [ view ], { type : 'audio/wav' } );
