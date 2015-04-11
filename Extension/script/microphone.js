d0b1abf7f03775ac80d53b0d588b67135fa15960
kcaluru <krishna_aluru@brown.edu>
2015-04-11 17:02:41 +0400

    UI tweaks

--- Extension/script/microphone.js
+++ Extension/script/microphone.js
@@ -1,9 +1,37 @@
-if (!navigator.getUserMedia) {
-	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-}
+// if (!navigator.getUserMedia) {
+// 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+// }
     
-if (navigator.getUserMedia){
-    navigator.getUserMedia({audio:true}, success, function(e) {
-    alert('Error capturing audio.');
-    });
-} else alert('getUserMedia not supported in this browser.');
+// if (navigator.getUserMedia){
+//     navigator.getUserMedia({audio:true}, success, function(e) {
+//     alert('Error capturing audio.');
+//     });
+// } else alert('getUserMedia not supported in this browser.');
+$(document).ready(function() {
+	$("#tt_info").innerHTML = "Press the microphone to record.";
+	var start_player = document.createElement('audio');
+	start_player.id = "short_audio1";
+	start_player.src = chrome.extension.getURL("../assets/sounds/start_recording.mp3");
+	start_player.type = "audio/mpeg";
+	document.body.appendChild(start_player);
+
+	var stop_player = document.createElement('audio');
+	stop_player.id = "short_audio2";
+	stop_player.src = chrome.extension.getURL("../assets/sounds/stop_recording.mp3");
+	stop_player.type = "audio/mpeg";
+	document.body.appendChild(stop_player);
+
+	var started = false;
+	$(".wit-microphone").click(function() {
+		if (started) {
+			stop_player.play();
+			$(".wit-microphone").removeClass("active");
+			started = false;
+		}
+		else {
+			start_player.play();
+			$(".wit-microphone").addClass("active");
+			started = true;
+		}
+	});
+});
